b5ad3021586198e64608e2e06661530e760c7a2b
derek <derek@sorcie.co.uk>
2023-11-25 19:04:59 +0000

    code tidy up moved a couple routines into subs readied for colur bars to be added in

--- Scroll.asm
+++ Scroll.asm
@@ -1,13 +1,14 @@
+//
+//  Sorcie's Homework
+//
+
 .label charBase = $3000
 .label sinBase = $3400
 .label spriteBase = $3800
 
-
-
 * = $02 "zeropage" virtual
-//scrolltextPointer:		.word $00
 bounceOn: .byte 0
-colourOn: .byte 0
+colourBarsOn: .byte 0
 sinPosition: .byte 0
 nextchar: .byte 0
 spriteColour: .byte 0
@@ -16,41 +17,27 @@ spriteColour: .byte 0
 
 BasicUpstart2(Start)
 
-
 Start: {
-
-	    //	sei 
-
-		//Stop CIA interrupts
-		// lda #$7f
-		// sta $dc0d
-		// sta $dd0d
-
-		//Bank out BASIC + Kernal
-		// lda #$35
-		// sta $01
-
-		//Set F/B colors and clear screen to black 
+		// Set Fore/back colors and clear screen to black 
+        // we can also set up some inital variables along the way while we have
+        // the correct number in a
 		lda #$00
         sta bounceOn
-        sta colourOn
+        sta colourBarsOn
         sta sinPosition
 		sta $d020
 		sta $d021
-        lda #1
-        sta spriteColour
+        lda #1              
+        sta spriteColour    //start sprite colour as white (really background since sprites are transparent)
 
         lda #<scrolltext
         sta PutCharInSprite7.scrolltextPointer
         lda #>scrolltext
         sta PutCharInSprite7.scrolltextPointer+1
-
-
-
 		jsr ClearScreen
 
         ldx #7
-        stx nextchar    // while we have 7 .. make use of it :)
+        stx nextchar    // while we have 7 may as well initialise the scroller
         ldy #SpriteStart/64 + 7
     setSpriteBase:
         tya   
@@ -65,72 +52,90 @@ Start: {
         sta $d01d       //set X double width
         lda #0
         sta $d017       // set Y to normal height
-
         ldx #0
-        
-
     setSpriteXY:
         sta $d000,x     //set sprite X
         tay             // save a
-        lda sinBase     // sprite Y initially row 128 (see sin table)
+        lda sinBase     // sprite Y start at row 128 (see sin table)
         sta $d001,x     //set sprite Y
-        tya             // get a back
+        tya             //get our save X position back
         clc
         adc #48        // move sprite X along 48 pixels (double width)
         inx
-        inx             // move to next X,Y pair
+        inx             // move to next sprite
         cpx #$10        // have we done all 8?
-        bne setSpriteXY // not yet ^
-        lda #%11000000  // sprite 6 & 7 Y > 255 so set bit 9
-        sta $d010       // and store it
+        bne setSpriteXY // not yet
+        lda #%11000000  // sprite 6 & 7 need bit 9 setting because they are on rigth side of screen (X > 255)
+        sta $d010
 
         lda #$FF
-        sta $d015       // enable all sprites
+        sta $d015       //turn all the sprites on
 
-        jsr PutCharInSprite7
+        jsr PutCharInSprite7    //put first char of our text into sprite 7
 
 forever:
-		//wait for near screen bottom edge
-		lda #$e0					
+		//wait for some point lower on the screen than we ever put sprites
+		lda #$e0
+    wait4Line240:					
 		cmp $d012
-		bcs *-3
-
+		bcs wait4Line240 
 
 //inc $d020
-        jsr scrollSprites
-        jsr bounceIt
+        jsr scrollSprites   // move everything left a bit
+        jsr bounceIt        // do the bouncy thing if enabled
 //dec $d020
 
-    waitTop:
+    wait4ScreenTop:                // wait until we are at top of screen
         lda $d011
-        bpl waitTop
+        bpl wait4ScreenTop
 
+        lda colourBarsOn
+        bne justOneColour
+
+    // do colour bars here
+
+
+    justOneColour:
+    //     ldx sinPosition
+    //     lda sinBase,x       // find which line our sprites start on
+    // waitForSpriteTop:   
+    //     cmp $d012           // and wait until we get there
+    //     bcs waitForSpriteTop
+    //     tax                 //remember which line we started on
+    //     lda spriteColour
+    //     sta $d021           //turn background colour on (sprites are transparent)
+    //     txa                 //get start line back
+    //     clc
+    //     adc #15             // not realy bottom of sprite but we only use 8 lines so turn colour back off before the bottom
+    // waitForSpriteBottom:
+    //     cmp $d012
+    //     bcs waitForSpriteBottom 
+    //     lda #0
+    //     sta $d021           //turn background colour off
+    jsr SetSpriteColour
+
+    jmp forever         //keep doing it again and again until user gets bored and closes vice64
+    rts                 //obligatory rts because you never know
+}
+
+SetSpriteColour:
+{
         ldx sinPosition
-        lda sinBase,x
-        //clc
-        //adc #1
-    waitForSpriteTop:
-        cmp $d012
+        lda sinBase,x       // find which line our sprites start on
+    waitForSpriteTop:   
+        cmp $d012           // and wait until we get there
         bcs waitForSpriteTop
-        tax
+        tax                 //remember which line we started on
         lda spriteColour
-        sta $d021
-        txa
+        sta $d021           //turn background colour on (sprites are transparent)
+        txa                 //get start line back
         clc
-        adc #12
+        adc #15             // not realy bottom of sprite but we only use 8 lines so turn colour back off before the bottom
     waitForSpriteBottom:
         cmp $d012
         bcs waitForSpriteBottom 
         lda #0
-        sta $d021
-
-
-
-        jmp forever
-
-        rts
-
-
+        sta $d021           //turn background colour off
 }
 
 bounceIt:
@@ -142,13 +147,12 @@ bounceIt:
 
     lda sinBase,x
     ldx #0 
-    bounce1:  
-        sta $d001,x     //set sprite Y
-        inx
-        inx             // move to next X,Y pair
-        cpx #$10       // have we done all 8?
-        bne bounce1 // not yet ^
-
+bounce1:  
+    sta $d001,x     //set sprite Y
+    inx
+    inx             // move to next sprite
+    cpx #$10        // have we done all 8?
+    bne bounce1     // not yet ^
 exit:
     rts
 }
@@ -159,66 +163,63 @@ PutCharInSprite7:
     sta charData 
     lda #>charBase
     sta charData + 1
-
-nextcharacter:
+getCharacter:
     lda scrolltextPointer: $C0DE
-    tay
-    //lda scrolltext,x    // get character
-    bne testforspecial1
-        //sta scrolltextPointer                // put it back to the start if we hit a 0
-        lda #<scrolltext
-        sta scrolltextPointer
-        lda #>scrolltext
-        sta scrolltextPointer+1
-        jmp nextcharacter
-    
-    
-    testforspecial1:
-        bpl notspecial
-        cmp #192
-        bcs testforspecial2
-        and #1
-        sta bounceOn
-        inc scrolltextPointer
-        bne nextcharacter
-        inc scrolltextPointer+1
-        jmp nextcharacter
+    tay                     // save the characterr
+    inc scrolltextPointer   //and move pointer to next character in the text
+    bne notPageBoundary
+    inc scrolltextPointer+1
+notPageBoundary:
+    tya                     //restore character to a
+    bne testforspecial1     //are we at the end of the text
+    lda #<scrolltext        //yep lets go back to the beginning
+    sta scrolltextPointer
+    lda #>scrolltext
+    sta scrolltextPointer+1
+    jmp getCharacter        //and get the next(first!) character
+testforspecial1:
+    cmp #128
+    bcc notspecial          //bit 7 not set so its just a character
+    cmp #192                //test for bounce control    
+    bcs testforspecial2 
+    and #1
+    sta bounceOn
+    jmp getCharacter
+testforspecial2:
+    cmp #224                //test for change colour
+    bcs colourbars
+    and #$3f
+    sta spriteColour
+    jmp getCharacter
+colourbars:                 //if we get here then its only option left - turn colour bars on/off
+    and #1
+    sta colourBarsOn
+    jmp getCharacter
 
-    testforspecial2:
-        and #$3f
-        sta spriteColour
-        inc scrolltextPointer
-        bne nextcharacter
-        inc scrolltextPointer+1
-        jmp nextcharacter
-
-    // colour switch goes here
-
-    notspecial:
-    
+notspecial:
     // work out where in char set it is
     // base + character * 8
-    and #%11100000  //get bits 6/7 
+//    tay             //save for later
+    and #%11100000  //get bits 5/6/7  to work out which page of font data we are on
     clc
-    rol     // 7 > C, 6 >7
-    rol     // 7>0 6>C
-    rol     // 7>1 6>0 put them in bits 0/1 (divide 64)
+    rol     
+    rol     
+    rol
     rol
     adc charData + 1
     sta charData + 1
-    //lda scrolltext,x         // get char again
-    tya
-    and #%00011111          // mask off bits 7/8
+    tya                 // get original number back
+    and #%00011111      // mask off bits 5/6/7 - we already dealt with them
     asl
     asl
-    asl     // * 8
+    asl                 // * 8 for offset from start of page to this characters data
     clc
     adc charData
-    sta charData    // char data now poitns to the 8 bytes of this char
-
+    sta charData        // char data now points to the 8 bytes of this char
 
     // sprite 7 data is at spritebase + 64 * 7
-    // put data in column 3 of sprite rows 2-9
+    // put data in column 3 of sprite rows 4-11  (right hand side of sprite)
+    // 3 bytes per line so +9 to point to row 4 . + 2 to point to column 3
     lda #<spriteBase + (64*7) + 9 + 2
     sta sprite7char 
     lda #>spriteBase + (64*7) + 9 + 2
@@ -240,15 +241,8 @@ copy8:
 
 scrollSprites:
 {
-    
-    // rol spriteBase + (64*7) + 2
-    // rol spriteBase + (64*7) + 1
-    // rol spriteBase + (64*7) + 0
-    // rol spriteBase + (64*6) + 2
-    // rol spriteBase + (64*6) + 1
-    // rol spriteBase + (64*6) + 0
-
-//inc $d020
+    // generate a whole bunch of rotate commands so we can shift bits left in the sprite data
+    //can probably do this shorter but we have loads of RAM and this is quickest
     sec
     .for (var j=4; j<12; j++)
     {
@@ -260,16 +254,11 @@ scrollSprites:
             }
         }
     }
-//dec $d020
-    dec nextchar
-    bpl exit
-        inc PutCharInSprite7.scrolltextPointer
-        bne skip
-        inc PutCharInSprite7.scrolltextPointer+1
-    skip:
-    jsr PutCharInSprite7
-    lda #7
+    dec nextchar    //count the pixels
+    bpl exit        //not moved 8 yet so get out of here
+    lda #7          //reset pixel counter
     sta nextchar
+    jsr PutCharInSprite7    //put next character into sprite7
 exit:    
     rts
 }
@@ -277,7 +266,7 @@ exit:
 ClearScreen: {
 		lda #$20
 		ldx #$00
-	!:
+	loop:
 		sta $0400, x
 		sta $0500, x
 		sta $0600, x
@@ -288,22 +277,22 @@ ClearScreen: {
 		sta $da00, x
 		sta $db00, x
 		inx
-		bne !-
+		bne loop
 		rts
 }
 
-scrolltext:     // chars above 127 trigger effects on /off  128/9 = bounce off/on, 192 + x = colour, 224/5 = multicolour on/oof
+scrolltext:     // chars above 127 trigger effects on /off  128/9 = bounce off/on, 192 + x = colour, 224/5 = colour bars on/off
 		.text "OldSkoolCoder gave us homework. A text scroller he said... "
         .byte 129
         .text " We thought it would be fun to add extra things."
         .byte 128
-        .text" We can turn it on and off at will."
+        .text" We can turn bounce on and off."
         .byte 129
         .text " We can change colour. Lets try"
         .byte 192+2
         .text " RED(Really? you call this RED), or maybe we want"
         .byte 192+7
-        .text " YELLOW. I suppose some of you might like"
+        .text " YELLOW. I've been told some people like the colour"
         .byte 192+14
         .text " Purple? "
         .text "Anyway, I want to do some other stuff but its work in progress and i might not have time so ..."
@@ -314,27 +303,12 @@ scrolltext:     // chars above 127 trigger effects on /off  128/9 = bounce off/o
 
 * = charBase "Charset"
 CharSet:
-
 .import binary "464charset-00-7F.bin"   // charset at $3000 - $33ff
-
-CharSetEnd:
-.label charlength = CharSetEnd - CharSet
-
 
 * = $3400 "SinTable"  // sin data starting at line 100  $3400-$34ff
 SinTable: 
-
 .import binary "sintab.bin"
-
-SinTableEnd:
-
 
 * = spriteBase "Sprites"  // 8 sprites $3800-$39ff
 SpriteStart: 
-
-.fill 64*8,$ff // all trans for now
-
-SpriteEnd:
-.label spriteLen = SpriteEnd - SpriteStart
-
-
+.fill 64*8,$ff // fill sprite data all pixels on
